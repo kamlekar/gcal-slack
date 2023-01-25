b6ebdfa4174f9a0562a299cefb1603adca871a0a
Venkateshwar <venkateshwar.kamlekar@everest.engineering>
2023-01-25 15:45:15 +0530

    📦 separate code into files

--- constants.js
+++ constants.js
@@ -1,4 +1,7 @@
 module.exports = {
   // use ngrok to have a https url so the web push notifications work
-  HOST: 'https://36ab-49-205-247-250.ngrok.io'
+  HOST: 'https://36ab-49-205-247-250.ngrok.io',
+  PORT: 3000,
+  // If modifying these scopes, delete token.json.
+  SCOPES: ['https://www.googleapis.com/auth/calendar.readonly']
 }
---
+++ listEvents.js
@@ -0,0 +1,28 @@
+const {google} = require('googleapis');
+
+/**
+ * Lists the next 10 events on the user's primary calendar.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+async function listEvents(auth) {
+  const calendar = google.calendar({version: 'v3', auth});
+  const res = await calendar.events.list({
+    calendarId: 'primary',
+    timeMin: new Date().toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime',
+  });
+  const events = res.data.items;
+  if (!events || events.length === 0) {
+    console.log('No upcoming events found.');
+    return;
+  }
+  console.log('Upcoming 10 events:');
+  events.map((event, i) => {
+    const start = event.start.dateTime || event.start.date;
+    console.log(`${start} - ${event.summary}`);
+  });
+}
+
+module.exports = listEvents;
