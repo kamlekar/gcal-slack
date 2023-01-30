5fb7a88285a7e8207fb11bc7752ca48ed6228d1d
Venkateshwar <venkateshwar.kamlekar@everest.engineering>
2023-01-30 22:20:13 +0530

    ✨ add clear status and update status with end time

--- src/google/events/triggers.js
+++ src/google/events/triggers.js
@@ -1,18 +1,21 @@
-const { setStatus } = require('../../slack/status/index');
+const { setStatus, clearStatus } = require('../../slack/status/index');
 const { authorize } = require('../auth/auth');
 const { getRunningEvents } = require('../events/fetch');
 
 function calendarEventWatchCallback(req, res) {
   try {
     const subscription = req.body;
-    // res.status(201).json({});
-    const payload = JSON.stringify({ title: "Hello World", body: "This is your first push notification" });
+
     authorize().then(async function (auth) {
       const events = await getRunningEvents(auth);
       events.forEach(event => {
         console.log("current event:", event.summary);
-        setStatus(event.summary);
+        setStatus(event.summary, event.end.moment);
       });
+
+      if (events.length === 0) {
+        clearStatus();
+      }
     });
     console.log('subscription: ', JSON.stringify(subscription));
   }
--- src/slack/status/index.js
+++ src/slack/status/index.js
@@ -1,13 +1,25 @@
 const { slack } = require('../service/api');
 
-function setStatus(text) {
+function setStatus(text, momentEndTime) {
   slack.post('/users.profile.set', {
     profile: {
-      status_text: text
+      status_text: text,
+      status_emoji: '',
+      status_expiration: momentEndTime.unix()
     }
   })
 };
 
+function clearStatus() {
+  slack.post('/users.profile.set', {
+    profile: {
+      status_text: '',
+      status_emoji: '',
+    }
+  })
+}
+
 module.exports = {
-  setStatus
+  setStatus,
+  clearStatus
 }
