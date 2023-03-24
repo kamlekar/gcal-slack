ba0ec57f97df57324d3ddbef87594c0b279b8def
Venkateshwar <venkateshwar.kamlekar@everest.engineering>
2023-03-25 00:51:30 +0530

    Add actual google auth library. move sample code to google sample file
    https://github.com/googleapis/google-api-nodejs-client/issues/2254#issuecomment-649860484

--- src/auth/google.js
+++ src/auth/google.js
@@ -1,70 +1,105 @@
-const fs = require('fs').promises;
+// Copyright 2012 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const fs = require('fs');
 const path = require('path');
-const { authenticate } = require('@google-cloud/local-auth');
+const https = require('https');
+const url = require('url');
+const opn = require('open');
+const destroyer = require('server-destroy');
+
 const { google } = require('googleapis');
+const { HOST } = require('../common/constants');
+const plus = google.plus('v1');
 const { SCOPES } = require('../common/constants');
 
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+const PORT = process.env.PORT;
 
 /**
- * Reads previously authorized credentials from the save file.
- *
- * @return {Promise<OAuth2Client|null>}
+ * To use OAuth2 authentication, we need access to a a CLIENT_ID, CLIENT_SECRET, AND REDIRECT_URI.  To get these credentials for your application, visit https://console.cloud.google.com/apis/credentials.
  */
-async function loadSavedCredentialsIfExist() {
-  try {
-    const content = await fs.readFile(TOKEN_PATH);
-    const credentials = JSON.parse(content);
-    return google.auth.fromJSON(credentials);
-  } catch (err) {
-    return null;
-  }
+const keyPath = path.join(__dirname, '../../credentials.json');
+let keys = { redirect_uris: [''] };
+if (fs.existsSync(keyPath)) {
+  keys = require(keyPath).web;
 }
 
 /**
- * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
- *
- * @param {OAuth2Client} client
- * @return {Promise<void>}
+ * Create a new OAuth2 client with the configured keys.
  */
-async function saveCredentials(client) {
-  const content = await fs.readFile(CREDENTIALS_PATH);
-  const keys = JSON.parse(content);
-  const key = keys.installed || keys.web;
-  const payload = JSON.stringify({
-    type: 'authorized_user',
-    client_id: key.client_id,
-    client_secret: key.client_secret,
-    refresh_token: client.credentials.refresh_token,
+const oauth2Client = new google.auth.OAuth2(
+  keys.client_id,
+  keys.client_secret,
+  keys.redirect_uris[0]
+);
+
+/**
+ * This is one of the many ways you can configure googleapis to use authentication credentials.  In this method, we're setting a global reference for all APIs.  Any other API you use here, like google.drive('v3'), will now use this auth client. You can also override the auth client at the service and method call levels.
+ */
+google.options({ auth: oauth2Client });
+
+/**
+ * Open an http server to accept the oauth callback. In this simple example, the only request to our webserver is to /callback?code=<code>
+ */
+async function authenticate() {
+  return new Promise((resolve, reject) => {
+    // grab the url that will be used for authorization
+    const authorizeUrl = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES.join(' '),
+    });
+
+    const options = {
+      key: fs.readFileSync('localhost-key.pem'),
+      cert: fs.readFileSync('localhost.pem')
+    };
+
+    const server = https
+      .createServer(options, async (req, res) => {
+        try {
+          if (req.url.indexOf('code=') > -1) {
+            const qs = new url.URL(req.url, HOST)
+              .searchParams;
+            res.end('Authentication successful! Please return to the console.');
+            server.destroy();
+            const { tokens } = await oauth2Client.getToken(qs.get('code'));
+            oauth2Client.credentials = tokens; // eslint-disable-line require-atomic-updates
+            resolve(oauth2Client);
+          }
+        } catch (e) {
+          reject(e);
+        }
+      })
+      .listen(PORT, () => {
+        // open the browser to the authorize url to start the workflow
+        opn(authorizeUrl, { wait: false }).then(cp => cp.unref());
+      });
+    destroyer(server);
   });
-  await fs.writeFile(TOKEN_PATH, payload);
 }
 
-/**
- * Load or request or authorization to call APIs.
- *
- */
-async function authorize() {
-  let client = await loadSavedCredentialsIfExist();
-  if (client) {
-    return client;
-  }
-  client = await authenticate({
-    scopes: SCOPES,
-    keyfilePath: CREDENTIALS_PATH,
-  });
-  if (client.credentials) {
-    await saveCredentials(client);
-  }
-  return client;
+async function runSample() {
+  // retrieve user profile
+  const res = await plus.people.get({ userId: 'me' });
+  console.log(res.data);
 }
+
+authenticate()
+  .then(client => runSample(client))
+  .catch(console.error);
 
 module.exports = {
-  loadSavedCredentialsIfExist,
-  saveCredentials,
-  authorize,
+  authorize: authenticate
 }
