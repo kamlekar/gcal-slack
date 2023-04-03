6ac19e9db8feaa9d6faaa22d87929401ac0fe67d
Venkateshwar <venkateshwar.kamlekar@everest.engineering>
2023-04-03 15:55:13 +0530

    🎫 keep authchecks for each call

--- src/integrations/drive/index.js
+++ src/integrations/drive/index.js
@@ -1,13 +1,13 @@
 const { auth } = require('../../auth');
+const { authCheck } = require('../../common/route-helper');
 const { uploadFile } = require('./upload');
 
 const driveRoutes = (app) => {
-  app.post('/drive/upload', (req, res) => {
-    const authClient = auth.getClient();
+  app.post('/drive/upload', (req, res) => authCheck(req, res).then((authClient) => {
     uploadFile(req, res, authClient).then((files) => {
       res.end(JSON.stringify(files));
     })
-  });
+  }));
 }
 
 module.exports = {
