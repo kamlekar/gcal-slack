f9c5f6aaad4ba2d38bf25720e56e7e8d31c6f004
Venkateshwar <venkateshwar.kamlekar@everest.engineering>
2023-04-21 00:08:08 +0530

    add nunjucs and index html page

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+import * as nunjucks from 'nunjucks';
 import { AppModule } from './app.module';
 
+dotenv.config();
+
+const ROOT_DIR: string = join(__dirname, '..');
+const IS_PRODUCTION: boolean = process.env.LOCAL !== 'true';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: NestExpressApplication = await NestFactory.create(AppModule);
+  const port = 3000;
+  const opts: nunjucks.ConfigureOptions = {
+    express: app,
+    autoescape: true,
+    watch: !IS_PRODUCTION,
+    noCache: !IS_PRODUCTION,
+  };
+
+  nunjucks.configure(join(ROOT_DIR, IS_PRODUCTION ? 'dist' : 'src'), opts);
+
+  app.enableCors();
+  app.set('trust proxy', 1);
+  app.set('view engine', 'njk');
+
+  await app.listen(port, () => {
+    console.log(`server running on ${port}`);
+  });
 }
 bootstrap();
